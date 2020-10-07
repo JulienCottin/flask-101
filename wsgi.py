bddc22bf74952eb418cacb429b400d755f004f86
Julien COTTIN <yandel77@wanadoo.fr>
2020-10-07 14:46:27 +0200

    flask

--- wsgi.py
+++ wsgi.py
@@ -5,15 +5,18 @@ from flask import jsonify
 from flask import Flask
 app = Flask(__name__)
 
+PRODUCTS = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'test' },
+}
+
+BASE_URL = '/api/v1'
+
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/api/v1/products')
-def api() :
-
-    PRODUCTS = {
-        1: { 'id': 1, 'name': 'Skello' },
-        2: { 'id': 2, 'name': 'Socialive.tv' },
-        }
+@app.route(f'{BASE_URL}/products', methods=['GET'])
+def read_many_products():
     return jsonify(PRODUCTS)
